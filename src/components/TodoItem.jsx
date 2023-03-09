69812fa20f4d232c01251e833bc137065b4b7824
Kushagra <kseth0808@gmail.com>
2023-03-09 14:50:19 +0530

    add

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <div className="form" onClick={()=>{
+      props.onChecked(props.id)
+    }}>
+      <li>{props.text}</li>
+      </div>
+  );
+}
+
+export default ToDoItem;
